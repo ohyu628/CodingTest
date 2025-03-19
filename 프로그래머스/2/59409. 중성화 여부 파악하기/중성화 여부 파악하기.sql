9ea6af92c67aeb6c0f16780df353a692504dead7
오유빈 <hhxx0083@gmail.com>
2025-03-19 15:27:28 +0900

    여러가지 풀이로 중성화 여부 파악하기.sql

--- 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
+++ 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
@@ -1,8 +1,36 @@
 -- 코드를 입력하세요
+# MySQL 버전
+# IF함수 사용
+SELECT ANIMAL_ID, NAME, 
+    IF(SEX_UPON_INTAKE LIKE '%Neutered%' OR SEX_UPON_INTAKE LIKE '%Spayed%', 'O', 'X') AS '중성화'
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID;
+
+# CASE문, 정규표현식 사용
+SELECT ANIMAL_ID, NAME, 
+    CASE 
+        WHEN REGEXP(SEX_UPON_INTAKE, 'Neutered|Spayed') THEN 'O'
+        ELSE 'X'
+    END AS '중성화'
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID;
+
+
+# Oracle 버전
+# CASE문, LIKE 연산자 사용
 SELECT ANIMAL_ID, NAME, 
     CASE
-        WHEN SEX_UPON_INTAKE LIKE 'Neutered%' THEN 'O'
-        WHEN SEX_UPON_INTAKE LIKE 'Spayed%' THEN 'O'
+        WHEN SEX_UPON_INTAKE LIKE '%Neutered%' THEN 'O'
+        WHEN SEX_UPON_INTAKE LIKE '%Spayed%' THEN 'O'
+        ELSE 'X'
+    END AS '중성화'
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID;
+
+# CASE문, 정규표현식 사용
+SELECT ANIMAL_ID, NAME, 
+    CASE 
+        WHEN REGEXP_LIKE(SEX_UPON_INTAKE, 'Neutered|Spayed') THEN 'O'
         ELSE 'X'
     END AS '중성화'
 FROM ANIMAL_INS
