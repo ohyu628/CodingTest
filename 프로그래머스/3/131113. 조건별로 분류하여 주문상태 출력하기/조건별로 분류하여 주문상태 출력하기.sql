49a301907bdefee9113b36ea6d01d16d862e1376
오유빈 <hhxx0083@gmail.com>
2025-04-24 19:54:21 +0900

    여러가지 풀이로 조건별로 분류하여 주문상태 출력하기.sql

--- 프로그래머스/3/131113. 조건별로 분류하여 주문상태 출력하기/조건별로 분류하여 주문상태 출력하기.sql
+++ 프로그래머스/3/131113. 조건별로 분류하여 주문상태 출력하기/조건별로 분류하여 주문상태 출력하기.sql
@@ -11,3 +11,12 @@ FROM FOOD_ORDER
 ORDER BY ORDER_ID;
 
 # Oracle 버전
+# 보여주는 것은 문자열로 변경해야하고 날짜와 비교하는 것은 날짜로 변경해줘야 함
+SELECT ORDER_ID, PRODUCT_ID, TO_CHAR(OUT_DATE, 'YYYY-MM-DD') AS OUT_DATE,
+    CASE
+        WHEN OUT_DATE <= TO_DATE('2022-05-01', 'YYYY-MM-DD') THEN '출고완료'
+        WHEN OUT_DATE >= TO_DATE('2022-05-01', 'YYYY-MM-DD') THEN '출고대기'
+        WHEN OUT_DATE IS NULL THEN '출고미정'
+    END AS "출고여부"
+FROM FOOD_ORDER
+ORDER BY ORDER_ID;
