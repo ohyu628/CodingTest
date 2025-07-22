314b345abb78174653b86cecedfff6e1a63572cf
오유빈 <hhxx0083@gmail.com>
2025-07-22 15:03:25 +0900

    [Bronze III] Title: 곱셈, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2588. 곱셈/곱셈.py
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -1,15 +1,3 @@
-# B를 list로 바꿔서 풀으려고 했지만 문자열로 받아 정수로 변경해주는게 더 쉬움
-# 방법 1
-# reversed() - 역순으로 뒤집는 함수
-A = int(input())
-B = input()
-
-for i in reversed(B):
-    print(A * int(i))
-print(A * int(B))
-
-# 방법 2
-# range(start, stop, step)
 A = int(input())
 B = input()
 
