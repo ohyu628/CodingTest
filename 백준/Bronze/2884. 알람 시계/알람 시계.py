bbf1423c3f16e3a76385b753e9c04dc0b754167b
오유빈 <hhxx0083@gmail.com>
2025-07-22 17:13:39 +0900

    Update 알람 시계.py

--- 백준/Bronze/2884. 알람 시계/알람 시계.py
+++ 백준/Bronze/2884. 알람 시계/알람 시계.py
@@ -1,3 +1,21 @@
+# H 1시간 빠지면 M 60분 추가
+# 방법 1
+H, M = map(int, input().split())
+if M < 45:
+    if H == 0:
+        H = 24
+        H -= 1
+        M += 60 - 45
+        print(H, M)
+    else:
+        H -= 1
+        M += 60 - 45
+        print(H, M)
+# M > 45 경우 출력값이 있어야 함
+else:
+    print(H, M-45)
+
+# 방법 2
 H, M = map(int, input().split())
 
 if M < 45 :	
