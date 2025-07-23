5426dbdbb9bc2cb833a2a659d83067a766ae356b
오유빈 <hhxx0083@gmail.com>
2025-07-23 13:04:15 +0900

    Update 합.py

--- 백준/Bronze/8393. 합/합.py
+++ 백준/Bronze/8393. 합/합.py
@@ -1,9 +1,14 @@
+# 방법 1
+# result = 누적합 저장할 변수
+# for문 안에 설정하면 매번 0으로 재설정되기 때문에 밖에 설정해줘야 함
 n = int(input())
 
-# 누적합 저장할 변수
-# for문 안에 설정하면 매번 0으로 재설정되기 때문에 밖에 설정해줘야 함
 result = 0
 
 for i in range(1, n+1):
     result += i
 print(result)
+
+# 방법 2
+n = int(input())
+print(sum(range(1, n+1)))
