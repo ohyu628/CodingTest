2f2d80f2b6549148d50566addb2078e97cfb7100
오유빈 <hhxx0083@gmail.com>
2025-07-23 23:12:31 +0900

    Update 별 찍기 － 1.py

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.py
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.py
@@ -1,4 +1,15 @@
+# 방법 1
 N = int(input())
 
 for i in range(1, N+1):
     print('*'*i)
+
+# 방법 2
+result = ""
+N = int(input())
+
+for i in range(N): # 열
+    for j in range(i+1): # 행
+        result += "*"
+    result += "\n"
+print(result)
