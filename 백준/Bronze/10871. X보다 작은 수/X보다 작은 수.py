bf802a9a122d3bfb84e3d08d6a4e7be2d21d93df
오유빈 <hhxx0083@gmail.com>
2025-07-26 09:46:15 +0900

    Update X보다 작은 수.py

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.py
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.py
@@ -1,3 +1,16 @@
+# 방법 1
+# int로 출력
+N, X = map(int, input().split())
+A = list(map(int, input().split()))
+
+lst = []
+for i in A:
+    if i < X:
+        lst.append(i)
+print(*lst)
+
+# 방법 2
+# string으로 출력
 N, X = map(int, input().split())
 A = list(map(int, input().split()))
 
