a64af32670dee6fb1241183101d601a2e106ed08
오유빈 <hhxx0083@gmail.com>
2025-07-30 16:04:43 +0900

    Update 상수.py

--- 백준/Bronze/2908. 상수/상수.py
+++ 백준/Bronze/2908. 상수/상수.py
@@ -1,9 +1,24 @@
-A, B = map(int, input().split())
+# 방법 1
+# 역슬라이싱
+A, B = input().split()
 
-A = int(str(A)[::-1])
-B = int(str(B)[::-1])
+A = int(A[::-1])
+B = int(B[::-1])
 
 if A > B:
     print(A)
 elif A < B:
     print(B)
+
+# 방법 2
+# reverse 함수 - 리스트에서만 사용 가능
+A, B = map(list, input().split())
+
+
+A.reverse()
+B.reverse()
+
+if ''.join(A) > ''.join(B):
+    print(''.join(A))
+else:
+    print(''.join(B))
