7959483e6ab4ce2f06b4fb5aa9638f64262b962d
오유빈 <hhxx0083@gmail.com>
2025-08-02 17:06:35 +0900

    Update 연월일 달력.py

--- SWEA/D1/2056. 연월일 달력/연월일 달력.py
+++ SWEA/D1/2056. 연월일 달력/연월일 달력.py
@@ -1,10 +1,10 @@
+# 방법 1
 T = int(input())
-
+days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 for tc in range(1, T+1):
     date = input()
 
     result = ''
-    days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     year = int(date[:4])
     month = int(date[4:6])
     day = int(date[6:])
@@ -23,3 +23,18 @@ for tc in range(1, T+1):
         result = "-1"
 
     print(f'#{tc} {result}')
+
+# 방법 2
+# year가 0000이 있으면 문제가 될 수 있음
+T = int(input())
+days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+for tc in range(1, T+1):
+    date = input()
+
+    month = int(date[4:6])
+    day = int(date[6:])
+    result = date[0:4] + '/' + date[4:6] + '/' + date[6:]
+    if (1 <= month <= 12) and (1 <= day <= days[month-1]):
+        print(f'#{tc} {result}')
+    else:
+        print(f'#{tc}', -1)
