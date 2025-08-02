299a5f0b12494c501b376e9a5bfe0d5b1e781328
오유빈 <hhxx0083@gmail.com>
2025-08-02 19:08:53 +0900

    Update 알파벳을 숫자로 변환.py

--- SWEA/D1/2050. 알파벳을 숫자로 변환/알파벳을 숫자로 변환.py
+++ SWEA/D1/2050. 알파벳을 숫자로 변환/알파벳을 숫자로 변환.py
@@ -1,3 +1,4 @@
+# 방법 1
 S = input()
 
 alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
@@ -7,3 +8,10 @@ for i in range(len(S)):
     if S[i] in alphabet:
         result = alphabet.index(S[i])
         print(number[result], end=' ')
+
+# 방법 2
+# ord 함수를 사용해 문자를 아스키코드로 변경
+# 아스키코드로 A가 64여서 -64를 해주면 1부터 출력
+S = input()
+for i in S:
+    print(ord(i)-64, end=' ')
