87f5659d3341d3ab470f09396864f576a901cd23
오유빈 <hhxx0083@gmail.com>
2025-08-02 18:52:28 +0900

    Update 거꾸로 출력해 보아요.py

--- SWEA/D1/1545. 거꾸로 출력해 보아요/거꾸로 출력해 보아요.py
+++ SWEA/D1/1545. 거꾸로 출력해 보아요/거꾸로 출력해 보아요.py
@@ -1,6 +1,13 @@
+# 방법 1
 N = int(input())
 
 result =[]
 for i in range(N,-1,-1):
     result.append(i)
 print(*result)
+
+# 방법 2
+N = int(input())
+
+for i in range(N, -1, -1):
+    print(i, end=" ")
