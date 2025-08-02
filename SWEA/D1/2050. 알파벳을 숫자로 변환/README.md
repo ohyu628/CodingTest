ce15d2941d2aa936c3ca5ff8850efbcf343457e8
오유빈 <hhxx0083@gmail.com>
2025-08-02 19:03:49 +0900

    [D1] Title: 알파벳을 숫자로 변환, Time: 53 ms, Memory: 53,632 KB -BaekjoonHub

---
+++ SWEA/D1/2050. 알파벳을 숫자로 변환/README.md
@@ -0,0 +1,15 @@
+# [D1] 알파벳을 숫자로 변환 - 2050 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5QLGxKAzQDFAUq) 
+
+### 성능 요약
+
+메모리: 53,632 KB, 시간: 53 ms, 코드길이: 386 Bytes
+
+### 제출 일자
+
+2025-08-02 19:03
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D1/2050. 알파벳을 숫자로 변환/알파벳을 숫자로 변환.py
@@ -0,0 +1,9 @@
+S = input()
+
+alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]
+
+for i in range(len(S)):
+    if S[i] in alphabet:
+        result = alphabet.index(S[i])
+        print(number[result], end=' ')
