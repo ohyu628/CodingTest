5e04085581bf08427fa9ab9ed3830c7b321cd244
오유빈 <hhxx0083@gmail.com>
2025-08-04 21:45:46 +0900

    [D2] Title: [파이썬 S/W 문제해결 기본] 1일차 - min max, Time: 59 ms, Memory: 53,760 KB -BaekjoonHub

--- SWEA/D2/4828. ［파이썬 S／W 문제해결 기본］ 1일차 － min max/［파이썬 S／W 문제해결 기본］ 1일차 － min max.py
+++ SWEA/D2/4828. ［파이썬 S／W 문제해결 기본］ 1일차 － min max/［파이썬 S／W 문제해결 기본］ 1일차 － min max.py
@@ -2,7 +2,14 @@ T = int(input())
 
 for tc in range(1, T+1):
     N = int(input())
-    
     a = list(map(int, input().split()))
 
-    print(f'#{tc} {max(a)-min(a)}')
+    max_value = 0
+    min_value = a[0]
+    for i in range(len(a)):
+        if a[i] > max_value:
+            max_value = a[i]
+        elif a[i] < min_value:
+            min_value = a[i]
+
+    print(f'#{tc} {max_value - min_value}')
