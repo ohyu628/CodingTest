e6ce81c07cc50ad1682910866d5e6875255930ac
오유빈 <hhxx0083@gmail.com>
2025-08-09 00:59:46 +0900

    [D2] Title: 파리 퇴치, Time: 68 ms, Memory: 58,240 KB -BaekjoonHub

--- SWEA/D2/2001. 파리 퇴치/파리 퇴치.py
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.py
@@ -2,15 +2,17 @@ T = int(input())
 
 for tc in range(1, T+1):
     N, M = map(int, input().split())
-    arr = [list(map(int, input().split())) for _ in range(N)]
+    flies = [list(map(int, input().split())) for _ in range(N)]
 
     max_v = 0
     for i in range(N-M+1):
         for j in range(N-M+1):
-            total = 0
-            for r in range(M):
-                for c in range(M):
-                    total += arr[i+r][j+c]
-            if max_v < total:
-                max_v = total
+            # 파리채 한번의 총합
+            temp_sum = 0
+            for k in range(M):
+                for l in range(M): 
+                    temp_sum += flies[i+k][j+l]
+            # 둘중에 큰 값 비교하여 재할당
+            if max_v < temp_sum:
+                max_v = temp_sum
     print(f'#{tc} {max_v}')
