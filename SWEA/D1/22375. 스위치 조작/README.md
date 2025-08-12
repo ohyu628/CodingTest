0ceb9e486aaeb17dd87123147f23c48467aa0323
오유빈 <hhxx0083@gmail.com>
2025-08-12 17:47:32 +0900

    [D1] Title: 스위치 조작, Time: 69 ms, Memory: 52,608 KB -BaekjoonHub

---
+++ SWEA/D1/22375. 스위치 조작/README.md
@@ -0,0 +1,15 @@
+# [D1] 스위치 조작 - 22375 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AZHA7Cn6ZgsDFAQP) 
+
+### 성능 요약
+
+메모리: 52,608 KB, 시간: 69 ms, 코드길이: 419 Bytes
+
+### 제출 일자
+
+2025-08-12 17:47
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D1/22375. 스위치 조작/스위치 조작.py
@@ -0,0 +1,17 @@
+T = int(input())
+
+for tc in range(1, T+1):
+    N = int(input())
+    Ai = list(map(int, input().split()))
+    Bi = list(map(int, input().split()))
+
+    count = 0
+    for i in range(N):
+        if Ai[i] != Bi[i]:
+            for j in range(i, N):
+                if Bi[j] == 0:
+                    Bi[j] = 1
+                elif Bi[j] == 1:
+                    Bi[j] = 0
+            count += 1
+    print(f'#{tc} {count}')
