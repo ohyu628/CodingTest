c09b323bbe1a5acae44b77af5afa1b2f225c6ce8
오유빈 <hhxx0083@gmail.com>
2025-08-12 21:30:29 +0900

    Update 스위치 조작.py

--- SWEA/D1/22375. 스위치 조작/스위치 조작.py
+++ SWEA/D1/22375. 스위치 조작/스위치 조작.py
@@ -1,3 +1,5 @@
+# 방법 1
+# 델타 사용
 T = int(input())
 
 for tc in range(1, T+1):
@@ -13,5 +15,21 @@ for tc in range(1, T+1):
                     Bi[j] = 1
                 elif Bi[j] == 1:
                     Bi[j] = 0
+            count += 1
+    print(f'#{tc} {count}')
+
+# 방법 2
+T = int(input())
+
+for tc in range(1, T+1):
+    N = int(input())
+    Ai = list(map(int, input().split()))
+    Bi = list(map(int, input().split()))
+
+    count = 0
+    for i in range(N):
+        if Ai[i] != Bi[i]:
+            for j in range(i, N):
+                Bi[j] = 1 - Bi[j]
             count += 1
     print(f'#{tc} {count}')
