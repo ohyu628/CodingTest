5155c7c30fb42f1dbcb8bc1d7b15ea322b5ad64d
오유빈 <hhxx0083@gmail.com>
2025-08-16 18:08:22 +0900

    Update 백만 장자 프로젝트.py

--- SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.py
+++ SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.py
@@ -7,6 +7,7 @@ for tc in range(1, T+1):
     max_price = 0
     result = 0
 
+    # 뒤에서부터 max값 확인 후 갱신
     for i in range(N-1,-1, -1):
         if trading[i] > max_price:
             max_price = trading[i]
