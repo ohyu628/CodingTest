5a603eec1039c1e0b84317f266278d91c24554a2
오유빈 <hhxx0083@gmail.com>
2025-08-16 23:49:05 +0900

    Update 중간 평균값 구하기.py

--- SWEA/D2/1984. 중간 평균값 구하기/중간 평균값 구하기.py
+++ SWEA/D2/1984. 중간 평균값 구하기/중간 평균값 구하기.py
@@ -4,5 +4,6 @@ for tc in range(1, T+1):
     num = list(map(int, input().split()))
 
     sum_v = sum(num) - min(num) - max(num)
-
+    
+    # 반올림 round 함수 사용
     print(f'#{tc} {round(sum_v / (len(num) - 2))}')
