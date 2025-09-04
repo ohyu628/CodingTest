c3ae34d0a27d07747c530b2bdefec254c10b3589
오유빈 <hhxx0083@gmail.com>
2025-09-04 17:30:22 +0900

    Update 단지번호붙이기.py

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -36,3 +36,40 @@ house.sort()
 print(count)
 for i in range(len(house)):
     print(house[i])
+
+
+# DFS 풀이
+dr = [-1, 1, 0, 0]
+dc = [0, 0, -1, 1]
+
+def dfs(r, c):
+    global house_count
+    global city
+    for d in range(4):
+        nr = r + dr[d]
+        nc = c + dc[d]
+        if 0 <= nr < N and 0 <= nc < N and arr[nr][nc] == 1 and visited[nr][nc] == 0:
+            house_count += 1
+            visited[nr][nc] = 1
+            dfs(nr, nc)
+    pass
+
+N = int(input())
+arr = [list(map(int, input())) for _ in range(N)]
+visited = [[0]*N for _ in range(N)]
+city = 0
+house = []
+for i in range(N):
+    for j in range(N):
+        if arr[i][j] == 1 and visited[i][j] == 0:
+            house_count = 1
+            city += 1
+            visited[i][j] = 1
+            dfs(i, j)
+            house.append(house_count)
+
+house.sort()
+
+print(city)
+for i in range(len(house)):
+    print(house[i])
