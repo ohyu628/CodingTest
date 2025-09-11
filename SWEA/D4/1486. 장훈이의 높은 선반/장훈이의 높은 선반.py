6150a7fa0cdf7bc48bbac0fddb84768e440f582f
오유빈 <hhxx0083@gmail.com>
2025-09-11 21:11:46 +0900

    [D4] Title: 장훈이의 높은 선반, Time: 538 ms, Memory: 88,320 KB -BaekjoonHub

---
+++ SWEA/D4/1486. 장훈이의 높은 선반/장훈이의 높은 선반.py
@@ -0,0 +1,27 @@
+def top(n):
+    global result
+    if sum(path) >= B:
+        if result > sum(path) - B:
+            result = sum(path) - B
+        return
+
+    for i in range(n, N):
+        path.append(arr[i])
+        top(i+1)
+        path.pop()
+
+
+
+T = int(input())
+
+for tc in range(1, T+1):
+    N, B = map(int, input().split())
+    arr = list(map(int, input().split()))
+
+    path = []
+    visited = [0] * N
+    result = 1e9
+
+    top(0)
+
+    print(f'#{tc} {result}')
