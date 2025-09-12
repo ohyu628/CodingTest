7a9fc88451198193e4202fc5113b371d8753f8a0
오유빈 <hhxx0083@gmail.com>
2025-09-12 10:22:10 +0900

    Update 장훈이의 높은 선반.py

--- SWEA/D4/1486. 장훈이의 높은 선반/장훈이의 높은 선반.py
+++ SWEA/D4/1486. 장훈이의 높은 선반/장훈이의 높은 선반.py
@@ -1,22 +1,20 @@
+# 방법 1
 def top(n):
     global result
     if sum(path) >= B:
-        if result > sum(path) - B:
-            result = sum(path) - B
+        result = min(result, sum(path) - B)
         return
 
     for i in range(n, N):
-        path.append(arr[i])
+        path.append(height[i])
         top(i+1)
         path.pop()
-
-
 
 T = int(input())
 
 for tc in range(1, T+1):
     N, B = map(int, input().split())
-    arr = list(map(int, input().split()))
+    height = list(map(int, input().split()))
 
     path = []
     result = 1e9
@@ -24,3 +22,28 @@ for tc in range(1, T+1):
     top(0)
 
     print(f'#{tc} {result}')
+
+# 방법 2
+def recur(cnt, total_height):
+    global result
+    if total_height >= B:
+        result = min(result, total_height)
+        return
+    
+    if cnt == N:
+        return
+    
+    recur(cnt + 1, total_height + heights[cnt])
+    recur(cnt + 1, total_height)
+
+T = int(input())
+
+for tc in range(1, T+1):
+    N, B = map(int, input().split())
+
+    height = list(map(int, input().split()))
+    result = 10000 * N
+    
+    recur(0, 0)
+
+    print(f'#{tc} {min_answer - B}')
