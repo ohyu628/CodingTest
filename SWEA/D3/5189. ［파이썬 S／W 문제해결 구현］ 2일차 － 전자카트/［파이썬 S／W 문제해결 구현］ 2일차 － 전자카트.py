4247bff71995f168e7195cfd93c48b53620445da
오유빈 <hhxx0083@gmail.com>
2025-09-13 21:17:40 +0900

    Update ［파이썬 S／W 문제해결 구현］ 2일차 － 전자카트.py

--- SWEA/D3/5189. ［파이썬 S／W 문제해결 구현］ 2일차 － 전자카트/［파이썬 S／W 문제해결 구현］ 2일차 － 전자카트.py
+++ SWEA/D3/5189. ［파이썬 S／W 문제해결 구현］ 2일차 － 전자카트/［파이썬 S／W 문제해결 구현］ 2일차 － 전자카트.py
@@ -1,3 +1,33 @@
+# 방법 1 - 최적화 코드
+def recur(r, total, cnt):
+    global result
+    if cnt == N:
+        result = min(result, total + arr[r][0])
+        return
+    
+    for c in range(N):
+        if visited[j]:
+            continue
+        visited[c] = 1
+        recur(c, total + arr[r][c], cnt + 1)
+        visited[c] = 0
+
+T = int(input())
+
+for tc in range(1, T+1):
+    N = int(input())
+    arr = [list(map(int, input().split())) for _ in range(N)]
+
+    visited = [0] * N
+    visited[0] = 1
+    result = 1e9
+
+    # 현재 위치, 누적 배터리, 방문 구역 수
+    recur(0, 0, 1)
+        
+    print(f'#{tc} {result}')
+
+# 방법 2
 def recur(r, c, total):
     global result
     if all(visited):
