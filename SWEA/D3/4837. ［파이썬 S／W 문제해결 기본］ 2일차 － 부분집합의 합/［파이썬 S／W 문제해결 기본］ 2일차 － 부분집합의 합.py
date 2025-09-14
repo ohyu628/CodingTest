6e3c7c552266f1b36c23cc679804b9dee4b5b439
오유빈 <hhxx0083@gmail.com>
2025-09-14 20:35:26 +0900

    Update ［파이썬 S／W 문제해결 기본］ 2일차 － 부분집합의 합.py

--- SWEA/D3/4837. ［파이썬 S／W 문제해결 기본］ 2일차 － 부분집합의 합/［파이썬 S／W 문제해결 기본］ 2일차 － 부분집합의 합.py
+++ SWEA/D3/4837. ［파이썬 S／W 문제해결 기본］ 2일차 － 부분집합의 합/［파이썬 S／W 문제해결 기본］ 2일차 － 부분집합의 합.py
@@ -1,3 +1,4 @@
+# 방법 1
 def recur(cnt, idx):
     global count
     if cnt == N:
@@ -19,4 +20,26 @@ for tc in range(1, T+1):
     count = 0
 
     recur(0, 1)
+    print(f'#{tc} {count}')
+
+# 방법 2
+def recur(cnt, subset):
+    global count
+    if cnt == len(arr):
+        if sum(subset) == K and len(subset) == N:
+            count +=1
+        return
+
+    recur(cnt + 1, subset + [arr[cnt]])
+    recur(cnt + 1, subset)
+
+T = int(input())
+
+for tc in range(1, T+1):
+    N, K = map(int, input().split())
+
+    arr = [i for i in range(1, 13)]
+    count = 0
+
+    recur(0, [])
     print(f'#{tc} {count}')
