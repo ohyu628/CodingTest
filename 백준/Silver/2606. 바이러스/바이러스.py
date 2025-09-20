56b35d1f0a26aa733ee18e4dac99f1bd7cddeb30
오유빈 <hhxx0083@gmail.com>
2025-09-21 00:41:10 +0900

    [Silver III] Title: 바이러스, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -0,0 +1,25 @@
+def dfs(idx):
+    global count
+    for i in adj_list[idx]:
+        if visited[i]:
+            continue
+        visited[i] = 1
+        dfs(i)
+        count += 1
+
+computer = int(input())
+connect = int(input())
+
+adj_list = [[] for _ in range(computer + 1)]
+visited = [0] * (computer + 1)
+count = 0
+
+for _ in range(connect):
+    a, b = map(int, input().split())
+    adj_list[a].append(b)
+    adj_list[b].append(a)
+
+visited[1] = 1
+dfs(1)
+
+print(count)
