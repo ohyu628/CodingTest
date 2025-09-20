37415d59147daddf02176bb03ee31af01055f4de
오유빈 <hhxx0083@gmail.com>
2025-09-21 00:49:08 +0900

    Update 바이러스.py

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,3 +1,36 @@
+# bs
+from collections import deque
+
+computer = int(input())
+connect = int(input())
+
+adj_list = [[] for _ in range(computer + 1)]
+visited = [0] * (computer + 1)
+count = 0
+q = deque()
+
+for _ in range(connect):
+    a, b = map(int, input().split())
+    adj_list[a].append(b)
+    adj_list[b].append(a)
+
+visited[1] = 1
+q.append(1)
+
+while q:
+    idx = q.popleft()
+
+    for i in adj_list[idx]:
+        if visited[i]:
+            continue
+        visited[i] = 1
+        count += 1
+        q.append(i)
+
+print(count)
+
+
+# dfs
 def dfs(idx):
     global count
     for i in adj_list[idx]:
@@ -21,5 +54,6 @@ for _ in range(connect):
 
 visited[1] = 1
 dfs(1)
+
 
 print(count)
