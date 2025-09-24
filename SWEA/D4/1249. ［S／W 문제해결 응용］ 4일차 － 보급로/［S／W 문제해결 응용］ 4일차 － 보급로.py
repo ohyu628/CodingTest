26661fabd06d6c5a3327e17efc515af3479ec79a
오유빈 <hhxx0083@gmail.com>
2025-09-24 22:02:59 +0900

    Update ［S／W 문제해결 응용］ 4일차 － 보급로.py

--- SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.py
+++ SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.py
@@ -1,3 +1,41 @@
+# 방법 1 - dijkstra
+from heapq import heappop, heappush
+
+dr = [-1, 1, 0, 0]
+dc = [0, 0, -1, 1]
+
+T = int(input())
+
+for tc in range(1, T+1):
+    N = int(input())
+
+    arr = [list(map(int, input())) for _ in range(N)]
+
+    dists = [[1e9] * N for _ in range(N)]
+    dists[0][0] = 0
+
+    pq = [(0, 0, 0)]
+
+    while pq:
+        dist, r, c = heappop(pq)
+
+        if dists[r][c] < dist:
+            continue
+
+        for d in range(4):
+            nr = r + dr[d]
+            nc = c + dc[d]
+            if 0 <= nr < N and 0 <= nc < N:
+                new_cost = dist + arr[nr][nc]
+
+                if dists[nr][nc] > new_cost:
+                    dists[nr][nc] = new_cost
+                    heappush(pq, (new_cost, nr, nc))
+
+    print(f'#{tc} {dists[N-1][N-1]}')
+
+
+# 방법 2 - bfs
 from collections import deque
 
 dr = [-1, 1, 0, 0]
