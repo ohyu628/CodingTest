911dfc2aab5a604a4c28465779c83a4382e30cbf
오유빈 <hhxx0083@gmail.com>
2025-09-28 18:16:53 +0900

    Update ［파이썬 S／W 문제해결 구현］ 7일차 － 최소 신장 트리.py

--- SWEA/D4/5249. ［파이썬 S／W 문제해결 구현］ 7일차 － 최소 신장 트리/［파이썬 S／W 문제해결 구현］ 7일차 － 최소 신장 트리.py
+++ SWEA/D4/5249. ［파이썬 S／W 문제해결 구현］ 7일차 － 최소 신장 트리/［파이썬 S／W 문제해결 구현］ 7일차 － 최소 신장 트리.py
@@ -33,3 +33,49 @@ for tc in range(1, T+1):
 
             heappush(pq, (graph[node][next_node], next_node))
     print(f'#{tc} {min_v}')
+
+
+# Kruskal
+def find_set(x):
+    if x == parents[x]:
+        return x
+    parents[x] = find_set(parents[x])
+    return parents[x]
+
+def union(x, y):
+    rx = find_set(x)
+    ry = find_set(y)
+
+    if rx == ry:
+        return
+    
+    if rx < ry:
+        parents[ry] = rx
+    else:
+        parents[rx] = ry
+
+T = int(input())
+
+for tc in range(1, T+1):
+    V, E = map(int, input().split())
+    graph = []
+    for _ in range(E):
+        start, end, weight = map(int, input().split())
+        graph.append((start, end, weight))
+
+    graph.sort(key=lambda x: x[2])
+
+    cnt = 0
+    min_v = 0
+
+    parents = [i for i in range(V+1)]
+
+    for s, e, w in graph:
+        if find_set(s) != find_set(e):
+            union(s, e)
+            cnt += 1
+            min_v += w
+            
+            if cnt == V:
+                break
+    print(f'#{tc} {min_v}')
