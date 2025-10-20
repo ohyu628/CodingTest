6a8ca6cecdb65549baa19a0f232ed90b9cf48ed6
오유빈 <hhxx0083@gmail.com>
2025-10-20 22:29:43 +0900

    [Bronze V] Update 팩토리얼 2.py

--- 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.py
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.py
@@ -9,3 +9,23 @@ def factorial(cnt):
 
 result = factorial(N)
 print(result)
+
+# 방법 2
+N = int(input())
+
+def factorial(cnt):
+    if cnt > 1:
+        return cnt * factorial(cnt - 1)
+    else:
+        return 1
+
+result = factorial(N)
+print(result)
+
+# 방법 3
+import math
+
+N = int(input())
+
+result = math.factorial(N)
+print(result)
