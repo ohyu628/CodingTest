84a5f9594e0100b225543809458d8b890645de01
오유빈 <hhxx0083@gmail.com>
2026-01-09 16:32:53 +0900

    [Bronze I] Title: 단어 공부, Time: 72 ms, Memory: 34368 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -0,0 +1,11 @@
+word = input().upper()
+word_set = list(set(word))
+
+cnt = []
+for i in word_set:
+    cnt.append(word.count(i))
+
+if cnt.count(max(cnt)) > 1:
+    print('?')
+else:
+    print(word_set[cnt.index(max(cnt))])
